36e867c6d75694629eb010e64ca72a1a88fe2f74
Vinh Nguyen <ngocvinh.nnv@gmail.com>
2017-12-25 08:56:29 +0700

    Add autoencoders

---
+++ ann/6-autoencoders/init.py
@@ -0,0 +1,76 @@
+from __future__ import division, print_function, absolute_import, unicode_literals
+
+
+import os
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+import matplotlib
+import matplotlib.pyplot as plt
+plt.rcParams['axes.labelsize'] = 14
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['ytick.labelsize'] = 12
+
+from IPython.display import clear_output, Image, display, HTML, SVG
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("/tmp/data/")
+
+
+def reset_graph(seed=42):
+    tf.reset_default_graph()
+    tf.set_random_seed(seed)
+    np.random.seed(seed)
+    
+def show_image(filename, width='60%'):
+    return Image(filename=filename, width=width)
+
+def plot_image(image, shape=[28, 28]):
+    plt.imshow(image.reshape(shape), cmap="Greys", interpolation="nearest")
+    plt.axis("off")
+  
+def plot_multiple_images(images, n_rows, n_cols, pad=2):
+    images = images - images.min()  # make the minimum == 0, so the padding looks white
+    w,h = images.shape[1:]
+    image = np.zeros(((w+pad)*n_rows+pad, (h+pad)*n_cols+pad))
+    for y in range(n_rows):
+        for x in range(n_cols):
+            image[(y*(h+pad)+pad):(y*(h+pad)+pad+h),(x*(w+pad)+pad):(x*(w+pad)+pad+w)] = images[y*n_cols+x]
+    plt.imshow(image, cmap="Greys", interpolation="nearest")
+    plt.axis("off")
+
+def strip_consts(graph_def, max_const_size=32):
+    """Strip large constant values from graph_def."""
+    strip_def = tf.GraphDef()
+    for n0 in graph_def.node:
+        n = strip_def.node.add() 
+        n.MergeFrom(n0)
+        if n.op == 'Const':
+            tensor = n.attr['value'].tensor
+            size = len(tensor.tensor_content)
+            if size > max_const_size:
+                tensor.tensor_content = "b<stripped %d bytes>"%size
+    return strip_def
+
+def show_graph(graph_def, max_const_size=32):
+    """Visualize TensorFlow graph."""
+    if hasattr(graph_def, 'as_graph_def'):
+        graph_def = graph_def.as_graph_def()
+    strip_def = strip_consts(graph_def, max_const_size=max_const_size)
+    code = """
+        <script>
+          function load() {{
+            document.getElementById("{id}").pbtxt = {data};
+          }}
+        </script>
+        <link rel="import" href="https://tensorboard.appspot.com/tf-graph-basic.build.html" onload=load()>
+        <div style="height:600px">
+          <tf-graph-basic id="{id}"></tf-graph-basic>
+        </div>
+    """.format(data=repr(str(strip_def)), id='graph'+str(np.random.rand()))
+
+    iframe = """
+        <iframe seamless style="width:1200px;height:620px;border:0" srcdoc="{}"></iframe>
+    """.format(code.replace('"', '&quot;'))
+    display(HTML(iframe))
